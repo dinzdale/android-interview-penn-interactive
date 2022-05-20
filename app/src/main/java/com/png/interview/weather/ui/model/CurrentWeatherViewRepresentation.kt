ee6bc5e5dea22d7df5ca339afc69dfcd6e685e47
Gary Jacobs <garyjacobs@Garys-MacBook-Pro.local>
2022-05-20 14:05:20 -0400

    Story 1 - Location error entry.

--- app/src/main/java/com/png/interview/weather/ui/model/CurrentWeatherViewRepresentation.kt
+++ app/src/main/java/com/png/interview/weather/ui/model/CurrentWeatherViewRepresentation.kt
@@ -3,5 +3,5 @@ package com.png.interview.weather.ui.model
 sealed class CurrentWeatherViewRepresentation {
     class AvailableWeatherViewRep(val data: AvailableWeatherViewData) : CurrentWeatherViewRepresentation()
     object Empty : CurrentWeatherViewRepresentation()
-    object Error : CurrentWeatherViewRepresentation()
+    class Error(val data: String = "Canned Error Message") : CurrentWeatherViewRepresentation()
 }
